06a3c57e28efd96935b912d7ad5e888b54a9d7b2
SUYUANJIAN <1249383162@qq.com>
2019-03-16 14:47:13 +0800

    add function: INSERT

--- application/index/controller/TeacherController.php
+++ application/index/controller/TeacherController.php
@@ -1,6 +1,7 @@
 <?php
 namespace app\index\controller;
 use think\Controller;
+use think\Request;
 use app\common\model\Teacher;
 
 class TeacherController extends Controller
@@ -11,5 +12,24 @@ class TeacherController extends Controller
 		$teachers = $Teacher->select();
 		$this->assign('teachers', $teachers);
 		return $this->fetch();
+	}
+	
+	public function insert()
+	{
+		$postData = Request::instance()->post();
+        
+		$Teacher = new Teacher; 
+		$Teacher->name = $postData['name'];
+		$Teacher->username = $postData['username'];
+		$Teacher->sex = $postData['sex'];
+		$Teacher->email = $postData['email'];
+		return $Teacher->save();
+		
+	}
+	
+	public function add()
+	{
+		return  $this->fetch();
+		
 	}
 }
